0a4394ecf4673349927aa6f0a9a76383cd899ca1
sakshamsneh <sakshamsneh@gmail.com>
2020-05-23 22:47:16 +0530

    Add flask changes

--- flask/main.py
+++ flask/main.py
@@ -2,14 +2,21 @@ import os
 import re
 from datetime import datetime
 import pandas as pd
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, jsonify
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+df = pd.DataFrame()
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'csv'}
+
+
+def start_df(filename):
+    global df
+    df = pd.read_csv(os.path.join(os.getcwd(), filename),
+                     index_col=0, encoding="cp1252")
 
 
 @app.route("/")
@@ -17,12 +24,11 @@ def allowed_file(filename):
 def home():
     return render_template("home.html")
 
-# New functions
+
 @app.route("/display/<filename>")
 def display(filename=None):
-    df = pd.read_csv(os.path.join(os.getcwd(), filename),
-                     index_col=0, encoding="cp1252")
-    data = df.head(20).to_html(
+    global df
+    data = df.head(100).to_html(
         classes="table table-hover table-dark table-responsive table-sm")
     collist = df.columns
     return render_template("table.html", data=data, collist=collist)
@@ -35,6 +41,21 @@ def upload_file():
         if f and allowed_file(f.filename):
             filen = secure_filename(f.filename)
             f.save(os.path.join(os.getcwd(), filen))
+            start_df(filen)
             return redirect(url_for('display', filename=filen))
         else:
             return redirect(url_for('home'))
+
+
+@app.route('/api/column', methods=['GET', 'POST'])
+def ret_values():
+    global df
+    column = request.args['column']
+    if request.method == 'GET':
+        return jsonify(collist=list(df[column].dropna().unique()))
+    elif request.method == 'POST':
+        # write/filter df on the column unique values, return/update table from here
+        values = request.args['values']
+        data = df[df[column].isin(values)].head(100).to_html(
+            classes="table table-hover table-dark table-responsive table-sm")
+        return jsonify(data=data)
--- flask/static/js/script.js
+++ flask/static/js/script.js
@@ -1,28 +1,66 @@
-var selectedcol = [];
+$('#csvfile').on('change', function () {
+    var fileName = $(this).val().replace('C:\\fakepath\\', " ");
+    $(this).next('.custom-file-label').html(fileName);
+})
 
-$(document).on('click', 'a', function () {
+
+$('#graphtype').on('dblclick', function () {
+    $('#graphselector').hide();
+});
+
+$('.graphitem').on('click', function () {
     $('#type').html("Graph Options for type " + this.id.toUpperCase());
+    if (this.id != 'pie')
+        $('#selcolumn').attr('multiple', 'true');
+    else
+        $('#selcolumn').removeAttr('multiple');
     $('#graphselector').show();
 });
 
-$('#graphtype').on('click', function () {
-    $('#graphselector').hide();
+function startmodal(collist) {
+    $('#selcolval').find('option').remove();
+    $.each(collist, function (_, text) {
+        $('#selcolval').append($('<option></option>').val(text).html(text))
+    });
+    $('#selectmodal').modal('show');
+}
+
+var $currentTable = null;
+var index = null;
+var selectedcol = [];
+var $column = null;
+
+$('#savesel').on('click', function () {
+    var selectedvalues = $('#selcolval').val();
+    if (selectedvalues.length != 0) {
+        $('#selectmodal').modal('hide');
+        // pass selectedvalues to /api/column, get updated table, add color to it
+        $currentTable.find('tr').each(function () {
+            $(this).find('th').eq(index).addClass('selected');
+            $(this).find('td').eq(index - 1).addClass('selected');
+        });
+        selectedcol.push($column);
+    } else {
+        $('.toast').toast('show');
+    }
 })
 
 $('th').on('click', function () {
-    var $currentTable = $(this).closest('table');
-    var index = $(this).index();
+    $currentTable = $(this).closest('table');
+    index = $(this).index();
     if (selectedcol.includes($(this).html())) {
+        // pass selectedcol to /api/column, get original table, remove color to it
         $currentTable.find('tr').each(function () {
             $(this).find('th').eq(index).removeClass('selected');
             $(this).find('td').eq(index - 1).removeClass('selected');
         });
         selectedcol = selectedcol.filter(f => f !== $(this).html());
     } else {
-        $currentTable.find('tr').each(function () {
-            $(this).find('th').eq(index).addClass('selected');
-            $(this).find('td').eq(index - 1).addClass('selected');
+        $column = $(this).html();
+        $.getJSON($SCRIPT_ROOT + '/api/column', {
+            column: $column,
+        }, function (data) {
+            startmodal(data.collist);
         });
-        selectedcol.push($(this).html());
     }
 });
