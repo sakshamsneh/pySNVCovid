c37f636d52160835a2f01089517bad6266b9b0f9
sakshamsneh <sakshamsneh@gmail.com>
2020-05-24 23:42:17 +0530

    Add chartjs

--- flask/static/js/script.js
+++ flask/static/js/script.js
@@ -87,6 +87,52 @@ $('th').on('click', function () {
     }
 });
 
+function show_graph(data) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    Chart.defaults.global.defaultFontColor = 'white';
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    },
+                    gridLines: {
+                        color: 'rgba(255, 255, 255, 0.7)'
+                    }
+                }]
+            },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+}
+
 $('#genbtn').on('click', function () {
     var $columnselected = $('#selcolumn').val();
     console.log($columnselected);
@@ -94,5 +140,6 @@ $('#genbtn').on('click', function () {
         graph_fields: JSON.stringify($columnselected),
     }, function (data) {
         console.log(data);
+        show_graph(data);
     });
 });
